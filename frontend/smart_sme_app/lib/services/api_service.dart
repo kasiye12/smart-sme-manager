9886d1f365790ecbfe647595fc7c474bb1bc989e
Kasu <kasu@email.com>
2026-05-06 16:10:07 +0300

    good

--- frontend/smart_sme_app/lib/services/api_service.dart
+++ frontend/smart_sme_app/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:3000/api';
+  static const String baseUrl = 'https://smart-sme-api.onrender.com/api';
   
   static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
